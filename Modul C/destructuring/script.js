f41cfb13e064fe6d402b0e574942b1116648c863
rubenkodehode <ruben1.kodehode@gmail.com>
2024-12-06 11:00:57 +0100

    deconstructuring commit 2

--- Modul C/destructuring/script.js
+++ Modul C/destructuring/script.js
@@ -38,12 +38,55 @@
 // const [color1, color2, ...rest] = colors();
 // console.log(color1, color2, rest);
 
-// Destructuring a matrix
-const matrix = [
-  [1, 2, 3, 4, 5, 6, 7, 8],
-  [9, 10, 11, 12, 13, 14, 15, 16],
-  [17, 18, 19, 20, 21, 22, 23, 24],
-  [25, 26, 27, 28, 29, 30, 31, 32],
-];
+// Destructuring a matrix (2 dimensional table)
+// const matrix = [
+//   [1, 2, 3, 4, 5, 6, 7, 8],
+//   [9, 10, 11, 12, 13, 14, 15, 16],
+//   [17, 18, 19, 20, 21, 22, 23, 24],
+//   [25, 26, 27, 28, 29, 30, 31, 32],
+// ];
 
-console.log(matrix[2][4]);
+// // console.log(matrix[2][4]);
+
+// // without destructuring
+// // matrix.forEach((element, index) => {
+// //   console.log(`Row: ${index + 1} Value: ${element[7]}`);
+// // });
+
+// // with destructuring
+// const position = [
+//   ["x", 89],
+//   ["y", 14],
+//   ["z", -15],
+// ];
+
+// position.forEach(([axis, value]) => {
+//   console.log(`Axis: ${axis} Value: ${value}`);
+// });
+
+// const matrix2 = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// matrix2.forEach(([cord1, cord2, cord3]) => {
+//   console.log(`${cord1}  ${cord2}  ${cord3}`);
+// });
+
+// destructuring of objects
+const user = {
+  firstName: "Skylar",
+  displayName: "Cibzo",
+  hobbies: ["Gaming", "Geography", "History"],
+  adress: {
+    streetName: "Nerlivegen",
+    streetNumber: 14,
+    postalCode: 6063,
+    city: "Hjørungavåg",
+  },
+  id: 45961203,
+};
+
+const { displayName, firstName, hobbies, id, adress } = user;
+console.log(displayName, firstName, hobbies, id, adress);
