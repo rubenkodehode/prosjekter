4d932050e3c5964c0d67ec6c9ae119168a24fa9a
rubenkodehode <ruben1.kodehode@gmail.com>
2024-12-17 10:30:12 +0100

    .

--- Modul C/api test/script.js
+++ Modul C/api test/script.js
@@ -28,20 +28,31 @@ document.getElementById("searchBtn").addEventListener("click", () => {
       }
     })
     .then(({ lat, lon, display_name }) => {
-      const auroraApiUrl = `https://api.auroras.live/v1/?type=ace&data=probability&lat=${lat}&long=${lon}`;
+      const auroraApiUrl = `https://api.auroras.live/v1/?type=ace&data=threeday&data=probability&lat=${lat}&long=${lon}`;
 
       console.log(auroraApiUrl);
       return fetch(auroraApiUrl)
         .then((response) => response.json())
         .then((auroraData) => {
+          const { date, colour, value } = auroraData;
+
+          const dateObj = new Date(date);
+
+          const formattedDate = dateObj.toLocaleString("nb-NO", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          });
+
           const resultDiv = document.getElementById("result");
           resultDiv.innerHTML = `<p><strong>Location:</strong> ${display_name}</p>
                                 <h4>Aurora Data:</h4>
-                                <pre>${JSON.stringify(
-                                  auroraData,
-                                  null,
-                                  2
-                                )}</pre>`;
+                                <p><strong>Date:</strong> ${formattedDate}</p>
+                                <p><strong>Colour:</strong> ${colour}</p>
+                                <p><strong>Value:</strong> ${value}</p>`;
         });
     })
     .catch((error) => {
