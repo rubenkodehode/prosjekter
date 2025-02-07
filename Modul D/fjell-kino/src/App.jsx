50529a68f6ef0bcd65f6e2fcc4bf490d38fae5c0
rubenkodehode <ruben1.kodehode@gmail.com>
2025-02-07 21:22:55 +0100

    why.

--- Modul D/fjell-kino/src/App.jsx
+++ Modul D/fjell-kino/src/App.jsx
@@ -11,18 +11,20 @@ function App() {
     <AccessibilityProvider>
       <ThemeProvider>
         <Router>
-          <Header />
-          <div className="mainContent">
-            <Routes>
-              <Route path="/" element={<Movies />} />
-              <Route path="/movies" element={<Movies />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/booking" element={<Booking />} />
-              <Route path="/upcomingMovies" element={<UpcomingMovies />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+          <div id="root">
+            <Header />
+            <div className="mainContent">
+              <Routes>
+                <Route path="/" element={<Movies />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/booking" element={<Booking />} />
+                <Route path="/upcomingMovies" element={<UpcomingMovies />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
-          <Footer />
         </Router>
       </ThemeProvider>
     </AccessibilityProvider>
